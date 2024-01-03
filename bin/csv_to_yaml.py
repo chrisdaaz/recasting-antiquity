24f628870f7e82aec1f44de896b8fd1c42d40834
chrisdaaz <24395592+chrisdaaz@users.noreply.github.com>
2024-01-02 23:16:40 -0600

    keep copy of helper script

---
+++ bin/csv_to_yaml.py
@@ -0,0 +1,18 @@
+#!/home/linuxbrew/.linuxbrew/bin/python3
+import csv
+import yaml
+
+def convert_csv_to_yaml(csv_file, yaml_file):
+    data = {}
+    data['object_list'] = []
+
+    with open(csv_file, 'r', encoding='utf-8') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            data['object_list'].append(row)
+
+    with open(yaml_file, 'w', encoding='utf-8') as file:
+        yaml.dump(data, file, default_flow_style=False, allow_unicode=True)
+
+# Replace 'input.csv' and 'output.yaml' with your CSV and desired YAML file names
+convert_csv_to_yaml('./manuscript/catalogue.csv', './output.yaml')
