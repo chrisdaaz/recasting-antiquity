f70a70e362edeaccb728a5cad96f993fc0ba32a4
chrisdaaz <24395592+chrisdaaz@users.noreply.github.com>
2024-01-06 11:46:40 -0600

    script to create catalog entries

---
+++ bin/create_object_entries.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+project_directory=$(pwd)
+
+catalog_directory='content/catalogue'
+
+for object in {1..52}; do
+    file="$project_directory/$catalog_directory/${object}.md"
+    echo "---" > $file
+    echo "layout: entry" >> $file
+    echo "order: 100.${object}" >> $file
+    echo "object:" >> $file
+    echo "    - id: ${object}" >> $file
+    echo "---" >> $file
+done
+
